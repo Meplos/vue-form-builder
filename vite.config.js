942f0ea22571bc01b47828826865912a17a36baf
meplos <alexxd64150@gmail.com>
2021-04-11 10:27:43 +0200

    WIP: add vite dependencies

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import {defineConfig} from 'vite'
+import vue from "@vitejs/plugin-vue"
+
+export default defineConfig({
+    build: {
+        cssCodeSplit: false,
+    },
+    optimizeDeps: {
+        include: [
+            'vue'
+        ]
+    },
+    plugins:[vue()]
+});
